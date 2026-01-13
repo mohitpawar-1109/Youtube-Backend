1e43c72e6338bc569c20945748b1aedf0b35c51c
Mohit Pawar <mohitpawar1109@gmail.com>
2026-01-13 12:48:58 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import connectDB from "./db/index.js";
+import app from "./app.js";
 import dotenv from "dotenv";
 dotenv.config({
     path: "./.env"
 });
 
 
-connectDB();
+connectDB()
+.then( () =>{
+    app.listen(process.env.PORT||8000, () => {
+        console.log(`Server is running on PORT ${process.env.PORT}`);
+    });
+})
+.catch((err) => {
+    console.error("Failed to connect to DB", err);
+});
 
---
+++ src/utils/ApiError.js
@@ -0,0 +1,15 @@
+class ApiError extends Error {
+    constructor(statusCode, message = "something went wrong", errors = [], statck = "") {
+        super(message);
+        this.statusCode = statusCode;
+        this.message = message;
+        this.errors = errors;
+        this.data = null
+        if (statck) {
+            this.stack = statck;
+        }else{
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
+export default ApiError; 
